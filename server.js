df079e8cf6cd8ec401e63c8c8d0418d40c2b93f2
Josh Kannenberg <joshkannenberg@MacBook-Pro.local>
2023-01-18 14:40:42 -0500

    m2

--- server.js
+++ server.js
@@ -1,38 +1,55 @@
-const express = require('express');
-const sendMail = require('./mail.js')
+const express = require("express");
+const sendMail = require("./mail.js");
 
 const app = express();
-const path = require('path');
-
+const path = require("path");
 
 const PORT = process.env.PORT || 3000;
 
 // Data parsing
-app.use(express.urlencoded({
-    extended: false
-}));
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
 app.use(express.json());
 
-app.post('/email', (req, res) => {
-    console.log('Data: ', req.body);
-    
-    const { name, email, subject, text } = req.body;
-    
-    sendMail(name, email, subject, text, function(err, data) {
-        if (err) {
-            res.status(500).json({ message: 'Internal Error' });
-        } else {
-            res.json({ message: 'Email Sent!' })
-        }
-    });
+app.post("/email", (req, res) => {
+  console.log("Data: ", req.body);
+
+  const { name, email, subject, text } = req.body;
+
+  sendMail(name, email, subject, text, function (err, data) {
+    if (err) {
+      res.status(500).json({ message: "Internal Error" });
+    } else {
+      res.json({ message: "Email Sent!" });
+    }
+  });
 });
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.post("/email", (req, res) => {
+  console.log("Data: ", req.body);
+
+  const { name, email, subject, text } = req.body;
+
+  sendMail2(name, email, subject, text, function (err, data) {
+    if (err) {
+      res.status(500).json({ message: "Internal Error" });
+    } else {
+      res.json({ message: "Email Sent!" });
+    }
+  });
 });
 
-app.use(express.static(__dirname + '/public', {
-    extensions: ['html', 'htm']
-}));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
-app.listen(PORT, () => console.log('Server is starting on PORT, ', PORT));
+app.use(
+  express.static(__dirname + "/public", {
+    extensions: ["html", "htm"],
+  })
+);
+
+app.listen(PORT, () => console.log("Server is starting on PORT, ", PORT));
